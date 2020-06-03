24d8ba02b8637993a22d078e40b3b3fba734bfb6
s0ahc <zhengrong.han@outlook.com>
2020-06-03 12:54:52 +0800

    Add the course of IListList, That course discuss the different of IList<T> and List<T>!

---
+++ 08-IListList/IListList.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace List
+{
+    public class Users  //Class Users；类 用户
+    {
+        public string Name;
+        public int Age;
+
+        // Constructor of two parameters; 两参数的构造函数
+        // Attention: There is no customized non-parameter constructor, so system will build a implicit non-parameter constructor with default
+        // The default values like(int 0/ double 0.0D/ bool false/ string null/ decmial 0.0M etc. ).
+        // 注意: 如果没有自定义无参构造器，系统会自动用默认值生成一个隐式构造器作为默认构造器
+        public Users(string _Name, int _Age)
+        {
+            Name = _Name;
+            Age = _Age;
+        }
+    }
+       
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Users U = new Users("jiang", 24);
+            
+            IList<Users> UILists = new List<Users>(); 
+            //千万要注意：等式的右边是List<Users>,而不是 IList<Users>,
+            //如果在List前面加一个I， 就会出现错误：抽象类或接口无法创建实例。
+            
+            UILists.Add(U);
+
+            U = new Users("wang", 22);
+            UILists.Add(U);
+
+            List<Users> I = ConvertIListToList<Users>(UILists);
+
+            Console.WriteLine(I[0].Name);
+            Console.WriteLine(I[1].Name);
+
+            Console.Read();
+        }
+
+        /// <summary>
+        /// 转换IList<T>为List<T>      // Convert IList<T> to List<T>
+        /// </summary>
+        /// <typeparam name="T">指定的集合中泛型的类型</typeparam> // Assign the type of generic inside the collection
+        /// <param name="gbList">需要转换的IList</param>    // The IList<T> that will be convert
+        /// <returns></returns>
+
+        // The static method of generic IList<T> transform
+        // 泛型转换的静态方法。
+        public static List<T> ConvertIListToList<T>(IList<T> gbList) where T : class   
+        {
+            if (gbList != null && gbList.Count >= 1)
+            {
+                List<T> list = new List<T>();
+
+                // Copy all elements of IList<T> to List<T>
+                // 将IList中的元素复制到List中
+                for (int i = 0; i < gbList.Count; i++)  
+                {
+                    T temp = gbList[i] as T;
+                    if (temp != null)
+                        list.Add(temp);
+                }
+                return list;
+            }
+            return null;
+        }
+    }
+}
