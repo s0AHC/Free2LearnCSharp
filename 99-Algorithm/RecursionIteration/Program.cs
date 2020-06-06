3722ac53d0efbebd49c9b1dede7ed66e63081447
s0AHC <zhengrong.han@outlook.com>
2020-06-06 10:22:14 +0800

    Add algorthm courses.

---
+++ 99-Algorithm/RecursionIteration/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RecursionIteration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=5;            
+            Console.WriteLine($"Recursion Method: {RecursionMethod(a)}");
+            Console.WriteLine($"Iteration Methos: {IterationMethod(a)}");
+            // Recursion method include iteration method, but iteration just iteration no recursion involved.
+        }
+
+        // Recursion Method
+        static int RecursionMethod(int n)
+        {
+            if(n>=1)
+            {
+                return n+RecursionMethod(n-1);
+            }
+            else{return 0;}
+        }
+
+        // Iteration Method
+        static int IterationMethod(int n)
+        {
+            int s=0;
+            for(int i=0;i<=n;i++)
+            {
+                s+=i;
+            }
+            return s;
+        }
+    }
+}
