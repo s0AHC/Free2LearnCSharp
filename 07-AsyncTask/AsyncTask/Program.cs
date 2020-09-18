1b3deeccb24a10674d3ebfaf8389579d78122f73
s0AHC <zhengrong.han@outlook.com>
2020-09-18 21:49:12 +0800

    Reformat for easy to read.

--- 07-AsyncTask/AsyncTask/Program.cs
+++ 07-AsyncTask/AsyncTask/Program.cs
@@ -12,24 +12,23 @@ namespace AsyncTask
             //Thread.Sleep(200);
 
             // 2nd Generation Threadpool: readuce the thread creation, readuce the system resouce comsuption
-            // Defect:  1. you can't control task's processing sequenance
+            // Defects:  1. you can't control task's processing sequenance
             //          2. you can't get the thread status: cancel/ exception/ finish
             // for (int i = 1; i <=5; i++)
             // {
             //     // you can' to control sequence of thread inside the ThreadPool since it is out-of-order。
             //     // System.Console.WriteLine($"Task No.:{i}" ); try enable this one!!!
-            //     ThreadPool.QueueUserWorkItem(new WaitCallback((obj) => {
-            //         Console.WriteLine($"in taskpool: 第{obj}个执行任务");
-            //     }),i);
+            //     ThreadPool.QueueUserWorkItem(new WaitCallback((obj) => {Console.WriteLine($"in taskpool: 第{obj}个执行任务");}),i);
             // }
             
             // 3rd generation: Task, there are three method to create and process task
             //  3.1 new a instance of Task，call Start() 
             Task taskMethod1 = new Task(() =>
-            {
-                //Thread.Sleep(200);
-                Console.WriteLine($"1st method: new Task(Lambda expression), the thread id is {Thread.CurrentThread.ManagedThreadId}");
-            });
+                {
+                    //Thread.Sleep(200);
+                    Console.WriteLine($"1st method: new Task(Lambda expression), the thread id is {Thread.CurrentThread.ManagedThreadId}");
+                }
+            );
             taskMethod1.Start();
 
             //  3.2 Task.Factory.StartNew(Action action)
